f0104314e6a6fe2756d63b52eebfa9f2fdf50e47
Bogdan <bsshul@gmail.com>
2023-09-15 16:56:40 +0300

    Add Card

--- app/categories/[id]/page.tsx
+++ app/categories/[id]/page.tsx
@@ -1,10 +1,44 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import { ColumnIcon, FilterIcon, GridIcon } from "../../components/Icons";
+import Card from "../../components/Card";
+import { useParams } from "next/navigation";
 
 const Category = () => {
+ const [grid, setGrid] = useState(true);
+ const { id } = useParams();
  return (
-  <div>
-   <h1>Категорія</h1>
-  </div>
+  <>
+   <div className="flex justify-between items-center">
+    <h1>Категорія {id}</h1>
+
+    <div className="flex gap-3">
+     <button className="p-3 bg-slate-200 rounded-xl flex gap-2">
+      <FilterIcon />
+     </button>
+
+     <button
+      onClick={() => setGrid(!grid)}
+      className="sm:hidden bg-slate-200 p-3 rounded-xl active:bg-slate-300 transition-all"
+     >
+      {grid ? <GridIcon /> : <ColumnIcon />}
+     </button>
+    </div>
+   </div>
+
+   <section
+    className={`mt-5 grid ${grid ? "grid-cols-2" : "grid-cols-1"
+     } sm:grid-cols-2 md:grid-cols-3 gap-5`}
+   >
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+   </section>
+  </>
  )
 }
 
