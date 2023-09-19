816991995fde55c881e44eb73594d3a128bce444
Bogdan <bsshul@gmail.com>
2023-09-19 10:26:42 +0300

    Fix styles

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -16,13 +16,13 @@ export default function Card() {
     quality={100}
    />
    <div className="mt-2">
-    <h3 className="px-4">Назва моделі завдовжки 25</h3>
+    <h3 className="px-4 text-sm">Назва моделі завдовжки 25</h3>
 
     <div className="mt-1 font-medium flex flex-col gap-2">
      <p className="text-lg px-4">
       2500<span className="text-sm"> грн</span>
      </p>
-     <button className="w-full bg-yellow-300 h-10 px-8 rounded-b-xl flex justify-center items-center transition-all hover:bg-yellow-400">
+     <button className="w-full bg-black text-white h-10 px-8 rounded-b-xl flex justify-center items-center transition-all hover:bg-gray-900">
       Купити
      </button>
     </div>
